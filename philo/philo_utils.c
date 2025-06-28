b8a78f49a36860b8378c1d1924c8911abe03b6d2
AdrianPMiro <adrian.pedrero.miro@gmail.com>
2025-06-28 14:03:54 +0200

    Philo

---
+++ philo/philo_utils.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adrian <adrian@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/18 15:30:16 by adrian            #+#    #+#             */
+/*   Updated: 2025/04/23 16:00:02 by adrian           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int		i;
+	int		sign;
+	long	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == 32)
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		result = result * 10 + (nptr[i] - '0');
+		if (result > 2147483647 && sign == 1)
+			return (-1);
+		if (result > 2147483648 && sign == -1)
+			return (0);
+		i++;
+	}
+	return ((int)(result * sign));
+}
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	size_t	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+int	safe_exit(t_table *table, int status)
+{
+	int	i;
+
+	i = 0;
+	printf("Error\n");
+	if (table)
+	{
+		if (table->forks)
+		{
+			while (i < table->n_philos)
+				pthread_mutex_destroy(&table->forks[i++]);
+			free(table->forks);
+		}
+		pthread_mutex_destroy(&table->print_locks);
+		pthread_mutex_destroy(&table->check_mutex);
+		if (table->philo)
+			free(table->philo);
+	}
+	return (status);
+}
